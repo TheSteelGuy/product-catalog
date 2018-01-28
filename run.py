8d13ece30a0b02e57cf3f3d043417db6c6f449b0
TheSteelGuy <collotuos0@gmail.com>
2018-01-28 18:42:46 +0300

    add application entry point

---
+++ run.py
@@ -0,0 +1,14 @@
+#run.py
+#coding:"utf-8"
+
+import os
+
+
+from myapp import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
+if __name__ == ('__main__'):
+     app.run()
+
