ce56ae0e2a37512791c9455d0a9c1d3956fc2144
TheSteelGuy <collotuos0@gmail.com>
2018-03-02 20:34:09 +0300

    add manage to the app

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 import os
 from flask_script import Manager
-#from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from myapp import create_app
-#from  myapp import db
+from  myapp import db
 
 config_name=os.getenv('APP_SETTINGS')
 app = create_app('development')
 
-#migrate = Migrate(app, db_)
+migrate = Migrate(app, db_)
 manager = Manager(app)
 
-#manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
    manager.run()
